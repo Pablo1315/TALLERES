0ab0cde47bbfc00a5e6916842682a963db793966
Miguel17-97 <56398767+Miguel17-97@users.noreply.github.com>
2020-12-06 16:17:52 -0500

    Iniciando el programa
    Se escribieron comentarios

---
+++ Desarrollo.cpp
@@ -0,0 +1,8 @@
+/*
+	Name: DESARROLLO DEL TALLER DE LISTAS
+	Copyright: 
+	Author: Miguel Angel Bañol David Duque y Pablo Emilio Hernandez
+	Date: 06/12/20 15:15
+	Description: Puntos: tipo 1: 1; tipo 2: 10; tipo 3: 1
+*/
+
