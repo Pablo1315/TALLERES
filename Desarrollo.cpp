7ad1a051aacf7b80d07db7efc328123367e7a4c4
Miguel17-97 <56398767+Miguel17-97@users.noreply.github.com>
2020-12-06 17:17:26 -0500

    Desarrollo del punto 1 tipo 3
    se desarrolla el triangulo de Pascal efectivamente.

--- Desarrollo.cpp
+++ Desarrollo.cpp
@@ -1,7 +1,7 @@
 /*
 	Name: DESARROLLO DEL TALLER DE LISTAS
 	Copyright: 
-	Author: Miguel Angel Bañol David Duque y Pablo Emilio Hernandez
+	Author: Miguel Angel Banol David Duque y Pablo Emilio Hernandez
 	Date: 06/12/20 15:15
 	Description: Puntos: tipo 1: 1; tipo 2: 10; tipo 3: 1
 */
